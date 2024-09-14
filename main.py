2b5183d2139e75515dc290b65ac86693f0104cb5
stishka1 <balva2010@gmail.com>
2024-09-14 18:03:47 +0300

    base

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/hotels")
 def main():
     return ("Hello world!")
+
+@app.put("/hotels/{hotel_id}")
+def update_all(hotel_id: str):
+    pass
+
+@app.patch("/hotels/{hotel_id}")
+def update_one(hotel_id: str):
+    pass
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
