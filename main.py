4a78dd64e633d4b9ef3b72f5ee2e399414f48cec
stishka1 <balva2010@gmail.com>
2024-09-15 15:51:47 +0300

    base

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 import uvicorn
 
 app = FastAPI()
 
-@app.get("/hotels")
+global hotels
+
+hotels = [
+    {
+        id: 1,
+        "name": "Resort&SPA",
+        "title": "Turkish"
+    },
+    {
+        id: 2,
+        "name": "Miami Beach",
+        "title": "Indonesia"
+    },
+    {
+        id: 3,
+        "name": "EcoLine Resort",
+        "title": "Gvinea Hotel"
+    }
+]
+
+@app.get("/")
 def main():
     return ("Hello world!")
 
-@app.put("/hotels/{hotel_id}")
-def update_all(hotel_id: str):
+@app.post("hotels")
+def add_hotel(id: int, title: str = Body(), name: str = Body()):
     pass
 
+@app.put("/hotels/{hotel_id}")
+def update_all(hotel_id: str, title: Body(), name: Body()):
+    global hotels
+    # hotel = {k, v in hotels}
+
 @app.patch("/hotels/{hotel_id}")
-def update_one(hotel_id: str):
+def update_one(hotel_id: str, title: Body(), name: Body()):
     pass
 
 
