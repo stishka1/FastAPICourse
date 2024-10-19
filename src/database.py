a1090faeb1c94ca4c50dcc6db1ba9b590553606e
stishka1 <balva2010@gmail.com>
2024-10-20 00:08:21 +0300

    connect to DB part2 fin

--- src/database.py
+++ src/database.py
@@ -2,3 +2,4 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from src.config import settings
 
 engine = create_async_engine(settings.DB_URL)
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
