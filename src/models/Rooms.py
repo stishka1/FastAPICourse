1ca7d2b4dbedb03879efbaf1d11198990e065fe6
stishka1 <balva2010@gmail.com>
2024-10-20 21:23:54 +0300

    hotels+rooms models

--- src/models/Rooms.py
+++ src/models/Rooms.py
@@ -1,9 +1,13 @@
 from src.database import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 
-class HotelsOrm(Base):
-    __tablename__ ="hotels"
+
+class RoomsOrm(Base):
+    __tablename__ ="rooms"
     id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(String(100))
-    location: Mapped[str]
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
+    title: Mapped[str]
+    description: Mapped[str | None]
+    price: Mapped[int]
+    quantity: Mapped[int]
---
+++ src/models/hotels.py
@@ -0,0 +1,9 @@
+from src.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+class HotelsOrm(Base):
+    __tablename__ ="hotels"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    location: Mapped[str]
