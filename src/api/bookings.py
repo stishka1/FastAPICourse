9a92f8332f1f7834f162836b22cde4522c382a06
stishka1 <balva2010@gmail.com>
2024-11-26 21:44:55 +0300

    get_all_bookings

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,27 +1,32 @@
-from datetime import date
-
 from fastapi import APIRouter, Request
 
 from src.api.dependencies import DBDep, UserDep
-from src.schemas.bookings import BookingAdd
+from src.schemas.bookings import BookingAdd, BookingAddRequest
 from src.services.auth import AuthService
 
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 
+# принцип такой: много параметров передаем в pydantic схему (создаем ее)
+# Таким образом в Body будет валидация пользовательских данных от pydantic
+# недостающие данные указываем в другой схеме (2 схемы)
+# соединяем 2 схемы путем раскрытия словаря или метода .model_dump()
 @router.post("", summary="Добавить бронирование номера")
-async def add_booking(db: DBDep, room_id: int, date_from: date, date_to: date, user_id: UserDep):
-    room_info = await db.rooms.get_one_or_none(id=room_id)
+async def add_booking(db: DBDep, user_id: UserDep, bookingData: BookingAddRequest):
+    room_info = await db.rooms.get_one_or_none(id=bookingData.room_id)
+    room_price = room_info.price
 
-    total = room_info.price * (date_to - date_from).days
-    booking_data = {
-        "room_id": room_id,
-        "user_id": user_id,
-        "date_from": date_from,
-        "date_to": date_to,
-        "price": total
-    }
-    _booking_data = BookingAdd(**booking_data)
+    _booking_data = BookingAdd(user_id=user_id, price=room_price, **bookingData.model_dump())
     booking = await db.bookings.add(_booking_data)
     await db.commit()
 
     return {"status": "200", "data": booking}
+
+@router.get("", summary="Получить бронирования всех пользователей")
+async def get_all_bookings(db: DBDep):
+    all_bookings = await db.bookings.get_all()
+    return {"status": "200", "data": all_bookings}
+
+@router.get("/me", summary="Получить мои бронирования")
+async def get_my_bookings(db: DBDep, user_id: UserDep):
+    my_bookings = await db.bookings.get_one_or_none(user_id=user_id)
+    return {"status": "200", "data": my_bookings}
--- src/schemas/bookings.py
+++ src/schemas/bookings.py
@@ -1,9 +1,21 @@
 from datetime import date
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+
+
+class BookingAddRequest(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
 
 class BookingAdd(BaseModel):
-    room_id: int
     user_id: int
+    room_id: int
     date_from: date
     date_to: date
     price: int
+
+class Booking(BookingAdd):
+    id: int
+
+    # при помощи паттерна DataMapper из SQL Alchemy преобразуем в Pydantic схему
+    model_config = ConfigDict(from_attributes=True)
